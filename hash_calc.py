b8f85209bfe5449440ff9a0633437ac455fd2a81
fgbruna <fgbruna@uc.cl>
2017-12-24 17:37:12 -0300

    gitignore update

--- hash_calc.py
+++ hash_calc.py
@@ -4,21 +4,23 @@ import sys
 import os
 from PyQt5 import uic, QtGui
 from back import HashCalc, CheckEvent
-from PyQt5.QtCore import pyqtSignal, QThread, QEvent
+from PyQt5.QtCore import pyqtSignal, QThread
 from PyQt5.QtWidgets import (QMessageBox,
-    QMainWindow, QApplication, QLabel, QStackedWidget, QComboBox, QFileDialog)
+                             QMainWindow, QApplication, QLabel, QStackedWidget, QComboBox, QFileDialog)
 
-mainwindow = uic.loadUiType(os.path.join("ui","hash_calc.ui"))
+mainwindow = uic.loadUiType(os.path.join("ui", "hash_calc.ui"))
+
 
 class Main(*mainwindow):
 
-    trigger_file = pyqtSignal(CheckEvent) # In charge of emitting the file path
+    # In charge of emitting the file path
+    trigger_file = pyqtSignal(CheckEvent)
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.icono = QtGui.QIcon()
-        self.icono.addFile(os.path.join("ui","hashtag.svg"))
+        self.icono.addFile(os.path.join("ui", "hashtag.svg"))
         self.setWindowIcon(self.icono)
         self.selected_file = None
         self.back = HashCalc(self)
@@ -27,30 +29,30 @@ class Main(*mainwindow):
         self.fileButton.clicked.connect(self.getfile)
         self.verifyButton.clicked.connect(self.verify)
         self.show()
-    
+
     def getfile(self):
-      fname = QFileDialog.getOpenFileName(self, 'Open file')
-      self.selectedFile = fname[0]
-      mode = self.comboHash.currentText()
+        fname = QFileDialog.getOpenFileName(self, 'Open file')
+        self.selectedFile = fname[0]
+        mode = self.comboHash.currentText()
 
     def display_result(self, boolean):
         message = "Sums match" if boolean else "Sums do not match"
-        msg = QMessageBox.information(self,"Message", message,QMessageBox.Ok, QMessageBox.Ok)
-    
+        msg = QMessageBox.information(
+            self, "Message", message, QMessageBox.Ok, QMessageBox.Ok)
+
     def verify(self):
         if self.selectedFile:
             mode = self.comboHash.currentText()
             _hash = self.hashEdit.text().strip()
-            self.trigger_file.emit(CheckEvent(self.selectedFile,mode, _hash))
+            self.trigger_file.emit(CheckEvent(self.selectedFile, mode, _hash))
         # else QmessageBox!
-
 
 
 def main():
     app = QApplication(sys.argv)
     ex = Main()
     sys.exit(app.exec_())
-    
+
 
 if __name__ == '__main__':
     main()
--- hashing_utilities.py
+++ hashing_utilities.py
@@ -1,6 +1,7 @@
 import hashlib
 import os
 
+
 def md5(fname):
     hash_md5 = hashlib.md5()
     with open(fname, "rb") as f:
@@ -8,12 +9,14 @@ def md5(fname):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
+
 def sha1(fname):
     hash_sha1 = hashlib.sha1()
     with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_sha1.update(chunk)
     return hash_sha1.hexdigest()
+
 
 def sha256(fname):
     hash_sha256 = hashlib.sha256()
@@ -23,13 +26,13 @@ def sha256(fname):
     return hash_sha256.hexdigest()
 
 
-
 def main():
     path = input("Give me the path to your file: ")
     checksum = sha1(path.strip())
     print("CHECKSUM: ", checksum)
     _sum = input("Gimme the sum provided by the dev: ")
     print("YES" if _sum == checksum else "NO :(")
+
 
 if __name__ == '__main__':
     main()
